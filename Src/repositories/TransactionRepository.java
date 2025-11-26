d53ecf38371a3eb3191c3e46ba4087c170cde0dc
HectorPetersen <hector.guldager.petersen@gmail.com>
2025-11-26 11:38:30 +0100

    transaction

--- Src/repositories/TransactionRepository.java
+++ Src/repositories/TransactionRepository.java
@@ -1,8 +1,16 @@
 package repositories;
 
-/* CSV I/O
-Her fortæller vi java - hvor kommer data fra?
- */
+import structure.Transaction;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class TransactionRepository {
+
 }
--- Src/structure/Transaction.java
+++ Src/structure/Transaction.java
@@ -1,34 +1,133 @@
 package structure;
 
-public class Transaction implements Comparable<Transaction>{
+/**
+ * INGEN MATEMATIK ELLER LOGIK
+ * Klassen definerer kun, hvad en Transaction er.
+ * Hver transaktion er en "kvittering" for en begivenhed.
+ */
+public class Transaction implements Comparable<Transaction> {
+
+    private int id;
+    private int userId;
+    private String date;
+    private String ticker;
+    private double price;
+    private String currency;
+    private String orderType;
+    private int quantity;
+
+    public Transaction(int id,
+                       int userId,
+                       String date,
+                       String ticker,
+                       double price,
+                       String currency,
+                       String orderType,
+                       int quantity) {
+        this.id = id;
+        this.userId = userId;
+        this.date = date;
+        this.ticker = ticker;
+        this.price = price;
+        this.currency = currency;
+        this.orderType = orderType;
+        this.quantity = quantity;
+    }
 
 
-    /* INGEN MATEMATIK ELLER LOGIK -
-    Her fortæller vi java:
-    Hvad er en transaction?
+    public Transaction() {
+    }
 
-    Vi skal ikke have nogen logik herinde.
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    /**
+     * compareTo:
+     * Sorterer først efter dato, og hvis datoen er ens, så efter id.
      */
-
-    int userID = User.ID; // abstraction -> vi skal ikke kalde på User class i alle transaktioner
-    int transactionID;
-    int transactionDate;
-    String ticker;
-    double price;
-    String currency;
-    String orderType; // buy/sell
-    int quantity;
-
-    public void getuserID(){
-    }
-    public void gettransactionID(){
-    }
-    public void gettransactionDate(){
+    @Override
+    public int compareTo(Transaction other) {
+        int dateCompare = this.date.compareTo(other.date);
+        if (dateCompare != 0) {
+            return dateCompare;
+        }
+        return Integer.compare(this.id, other.id);
     }
 
-
-
-    public int compareTo(Transaction o) {
-        return 0;
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", date='" + date + '\'' +
+                ", ticker='" + ticker + '\'' +
+                ", price=" + price +
+                ", currency='" + currency + '\'' +
+                ", orderType='" + orderType + '\'' +
+                ", quantity=" + quantity +
+                '}';
     }
 }
