ccd2ee9aa1261ee66ab26b04dab3136a7a555164
thistle <thxstle.branch@gmail.com>
2025-11-28 14:33:51 +0100

    bug fixes

--- Src/structure/User.java
+++ Src/structure/User.java
@@ -8,14 +8,13 @@ public class User implements Comparable<User>{
 
     Vi skal ikke have nogen logik herinde.
      */
-//
-    public int id; // kommentar, har ændret first/last name til FullName*
-    public String fullName;
-    public String email;
-    public int birthDate;
-    public double initialCashDKK;
-    public int createdAt;
-    public int lastUpdated;
+    private int id; // kommentar, har ændret first/last name til FullName*
+    private String fullName;
+    private String email;
+    private int birthDate;
+    private double initialCashDKK;
+    private int createdAt;
+    private int lastUpdated;
 
     public User (int id, String fullName, String email, int birthDate, double initialCashDKK, int createdAt, int lastUpdated) {
 
@@ -28,27 +27,14 @@ public class User implements Comparable<User>{
         this.lastUpdated = lastUpdated;
     }
     // har slettet void under vores getter, da getters aldrig skal stå i void siger google.
-    public int getId() {
-        return id;
-    }
-   public String getFullName() {
-       return fullName;
-   }
-   public String getEmail() {
-       return email;
-   }
-   public int getBirthDate() {
-        return birthDate;
-   }
-   public double getInitialCashDKK() {
-       return initialCashDKK;
-   }
-   public int getCreatedAt() {
-       return createdAt;
-   }
 
 
-
+    public int getId() { return id; }
+   public String getFullName() { return fullName; }
+   public String getEmail() { return email; }
+   public int getBirthDate() { return birthDate;}
+   public double getInitialCashDKK() { return initialCashDKK; }
+   public int getCreatedAt() { return createdAt; }
    public int getLastUpdated() {
         return lastUpdated;
    }
