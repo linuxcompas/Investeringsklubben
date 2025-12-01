9cc357d07821542932346e785fbb22b51f98d28c
thistle <thxstle.branch@gmail.com>
2025-12-01 13:43:18 +0100

    Cashbalance skal ikke være i user. Det er portfolio.

--- Src/repositories/UserRepository.java
+++ Src/repositories/UserRepository.java
@@ -35,10 +35,9 @@ public class UserRepository {
                 double initialCash = Double.parseDouble(parts[4].replace("-", ""));
                 int createdAt = Integer.parseInt(parts[5].replace("-", ""));
                 int lastUpdated = Integer.parseInt(parts[6].replace("-", ""));
-                int cashBalance = Integer.parseInt(parts[7].replace("-", ""));;
 
                 User u = new User(id, fullName, email, birthDate,
-                        initialCash, createdAt, lastUpdated, cashBalance);
+                        initialCash, createdAt, lastUpdated);
 
                 users.add(u);
             }
@@ -98,8 +97,7 @@ public class UserRepository {
                             String.valueOf(u.getBirthDate()),
                             String.valueOf(u.getInitialCashDKK()),
                             String.valueOf(u.getCreatedAt()),
-                            String.valueOf(u.getLastUpdated()),
-                            String.valueOf(u.getCashBalance())
+                            String.valueOf(u.getLastUpdated())
                     );
 
                     bw.write(line);
@@ -118,7 +116,6 @@ public class UserRepository {
         boolean found = false;
         for (User u : users) {
             if (u.getId() == id) {
-                u.setCashBalance((int) newBalance);
                 u.setLastUpdated(java.time.LocalDate.now().getYear()
                         + java.time.LocalDate.now().getMonthValue()
                         + java.time.LocalDate.now().getDayOfMonth());
